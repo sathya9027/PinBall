7a0f812cf40dacbb86a5a5a3b2b0e255497dd3de
Sathya Narayanan aka SlimeArts <103625211+sathya9027@users.noreply.github.com>
2022-07-10 14:02:41 +0530

    Add files via upload

---
+++ Source Code/Common.h
@@ -0,0 +1,49 @@
+#pragma once
+/*There are three package need
+to be install inorder to
+work with opengl fully....
+
+Tools->NuGet Package Manager->Package Manager Console
+
+1.) Install Opengl Package - install-package nupengl.core
+2.) Install Soil Package - install-package soil
+3.) Install GLM Package - install-package glm
+4.) Install FreeType Package - install-package freetype
+5.) Install Openal Package - Install-Package OpenAL.Soft -Version 1.19.1*/
+
+//opengl lib
+#pragma warning (disable : 4996)
+#pragma comment(lib,"OpenGL32.lib")
+
+//glfw include's
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+//soil include's
+#include <soil.h>
+
+//glm include's
+#include "glm/glm.hpp"
+#include "glm/gtc/matrix_transform.hpp"
+#include "glm/gtc/type_ptr.hpp"
+//common include's
+#include <cstdlib>
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <Windows.h>
+#include <time.h>
+#include <map>
+#include <array>
+#include <string>
+
+//namespace
+using namespace std;
+using namespace glm;
+
+//including header files
+/*Framework header files*/
+#include "Window.h"
+#include "Shader.h"
+#include "Sprite.h"
+#include "Camera.h"
---
+++ Source Code/Sprite.h
@@ -0,0 +1,35 @@
+#pragma once
+class Sprite
+{
+private:
+	GLuint VBO, EBO, texture;
+	GLuint fVBO;
+	mat4 model;
+	vec2 spriteSize;
+	vec2 spritePosition;
+	vec2 centre;
+	vec2 distance;
+	float width;
+	float height;
+	float rotation;
+	float X, Y, R, B;
+	bool isXAxis, isYAxis;
+	bool flip;
+public:
+	Sprite();
+	~Sprite();
+	bool RectangleCollision(Sprite sprite);
+	bool CircleCollision(Sprite sprite);
+	bool CentreCollision(Sprite sprite);
+	bool WindowCollision(Window window);
+	bool& setFlip();
+	float& setRotation();
+	void CreateBuffers(float width, float height);
+	void LoadTexture(const char* filename);
+	void Draw(Shader shader);
+	void Update(Shader shader);
+	vec2 getSize();
+	vec2 getPosition();
+	vec2 getCenter();
+	vec2& setPosition();
+};
